8ede93b7b9d1b426ed7a403fdc80b507f2b5d893
KenjxJJ <kenjxjj@gmail.com>
2021-02-05 17:16:45 +0300

    Add models of the visitors to the app

--- design_hub_app/models.py
+++ design_hub_app/models.py
@@ -1,3 +1,15 @@
+from typing import Text
 from django.db import models
+from django.db.models.fields import CharField, DateField, TextField
 
 # Create your models here.
+class Visitor(models.Model):
+    name = models.CharField(max_length=55)
+    temperature = models.DecimalField(max_digits=3,decimal_places=1) 
+    company = models.CharField(max_length=95)
+    id_number = models.CharField(max_length=255, blank=True)
+    telephone_number = models.CharField(max_length=15)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
