b1fa4844a9fd48c4c3f702647d8a238b39f36fa4
KenjxJJ <kenjxjj@gmail.com>
2021-02-05 22:50:44 +0300

    Display database info in the home view page

--- design_hub_app/models.py
+++ design_hub_app/models.py
@@ -7,7 +7,7 @@ class Visitor(models.Model):
     name = models.CharField(max_length=55)
     temperature = models.DecimalField(max_digits=3,decimal_places=1) 
     company = models.CharField(max_length=95)
-    id_number = models.CharField(max_length=255, blank=True)
+    id_number = models.CharField(max_length=12, blank=True)
     telephone_number = models.CharField(max_length=15)
     date = models.DateField(auto_now_add=True)
 
--- design_hub_app/views.py
+++ design_hub_app/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
+from .models import Visitor
 
 # Create your views here.
 
 # Send a home_page with details of visitors
 def home_page(request):
-    return render(request,'design_hub_app/homepage.html')
+    all_visitors =  Visitor.objects.all();
+    return render(request,'design_hub_app/homepage.html', {'visitors' : all_visitors })
 
 # Send a login page 
 def login_page(request):
